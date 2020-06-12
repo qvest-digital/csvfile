60da744fdb9bf580c4d515dd15cad069c7fbe885
mirabilos <t.glaser@tarent.de>
2020-06-12 22:59:53 +0200

    add from latest mvnparent

--- src/main/ancillary/mksrc.sh
+++ src/main/ancillary/mksrc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env mksh
 # -*- mode: sh -*-
 #-
-# Copyright © 2016, 2018, 2019
+# Copyright © 2016, 2017, 2018, 2019, 2020
 #	mirabilos <t.glaser@tarent.de>
 #
 # Provided that these terms and disclaimer and all copyright notices
@@ -24,23 +24,42 @@
 # initialisation
 export LC_ALL=C
 unset LANGUAGE
-PS4='++ '
-# check that we’re really run from mvn
-if [[ -z $MKSRC_RUN_FROM_MAVEN ]]; then
-	print -ru2 -- "[ERROR] do not call me directly, I am only used by Maven"
-	export -p
-	exit 1
-fi
-# initialisation
-unset GZIP
 set -e
 set -o pipefail
-cd "$(dirname "$0")/../../.."
-if [[ -e failed ]]; then
-	print -ru2 -- "[ERROR] do not build from incomplete/dirty tree"
-	print -ru2 -- "[INFO] a previous mksrc failed and you used its result"
+unset GZIP
+exec 8>&1 9>&2
+
+function errmsg {
+	set -o noglob
+	local x
+
+	print -ru9 -- "$1"
+	shift
+	IFS=$'\n'
+	set -- $*
+	for x in "$@"; do
+		print -ru9 -- "| $x"
+	done
+}
+function die {
+	errmsg "$@"
 	exit 1
-fi
+}
+
+# check that we’re really run from mvn
+[[ -n $MKSRC_RUN_FROM_MAVEN ]] || die \
+    'do not call me directly, I am only used by Maven' \
+    "$(export -p)"
+# initialisation
+cd "$(dirname "$0")"
+ancillarypath=$PWD
+. ./cksrc.sh
+cd "$parentpompath"
+
+[[ ! -e failed ]] || die \
+    'do not build from incomplete/dirty tree' \
+    'a previous mksrc failed and you used its result'
+
 # get project metadata
 <pom.xml xmlstarlet sel \
     -N pom=http://maven.apache.org/POM/4.0.0 -T -t \
@@ -48,9 +67,13 @@ fi
     -c /pom:project/pom:artifactId -n \
     -c /pom:project/pom:version -n \
     |&
-IFS= read -pr pgID
-IFS= read -pr paID
-IFS= read -pr pVSN
+e=0
+IFS= read -pr pgID || e=1
+IFS= read -pr paID || e=1
+IFS= read -pr pVSN || e=1
+[[ $e = 0 && -n $pgID && -n $paID && -n $pVSN ]] || die \
+    'could not get project metadata' \
+    "pgID=$pgID" "paID=$paID" "pVSN=$pVSN"
 # create base directory
 tbname=target/mksrc
 tzname=$paID-$pVSN-source
@@ -60,43 +83,67 @@ mkdir -p "$tgname"
 
 # performing a release?
 if [[ $IS_M2RELEASEBUILD = true ]]; then
-	# fail the build if dependency licence review has a to-do item:
-	# src/main/ancillary/ckdep.sh will fail the build when the list
-	# was not up-to-date, so we check only the current list
-	if grep -e ' TO''DO$' -e ' FA''IL$' src/main/ancillary/ckdep.lst; then
-		print -ru2 -- "[ERROR] licence review incomplete"
-		exit 1
+	# if the dependency list for licence review still has a to-do or
+	# fail item, fail the build (we can handle the current list sin‐
+	# ce ./ckdep.sh already fails the build for not up-to-date list)
+	if grep -e ' TO''DO$' -e ' FA''IL$' "$ancillarypath"/ckdep.lst; then
+		die 'licence review incomplete'
 	fi
 fi
 
 # check for source cleanliness
 x=$(git status --porcelain)
 if [[ -n $x ]]; then
-	print -ru2 -- "[ERROR] source tree not clean"
-	print -ru2 -- "[INFO] git status output follows:"
-	print -r -- "$x" | sed 's/^/[INFO]   /' >&2
+	errmsg 'source tree not clean; “git status” output follows:' "$x"
 	if [[ $IS_M2RELEASEBUILD = true ]]; then
 		:>"$tgname"/failed
 		:>"$tbname"/failed
-		print -ru2 -- "[WARNING] maven-release-plugin prepare, continuing anyway"
+		print -ru8 -- "[WARNING] maven-release-plugin prepare, continuing anyway"
 		cd "$tbname"
 		paxtar -M dist -cf - "$tzname"/f* | gzip -n9 >"../$tzname.tgz"
+		rm -f src.tgz
+		ln "../$tzname.tgz" src.tgz
 		exit 0
 	fi
 	exit 1
 fi
 
-# enable verbosity
-set -x
-
 # copy git HEAD state
+print -ru8 -- "copying source tree"
 git ls-tree -r --name-only -z HEAD | sort -z | paxcpio -p0du "$tgname/"
-ts=$(TZ=UTC git show --no-patch --pretty=format:%ad \
-    --date=format-local:%Y%m%d%H%M.%S)
+print -ru8 -- "trimming source tree"
+
+# omit what will anyway end up in depsrcs
+if [[ $drop_depsrc_from_mksrc != 0 ]]; then
+	rm -rf "$tgname/$depsrcpath"
+fi
 
 # create source tarball
+ts=$(TZ=UTC git show --no-patch --pretty=format:%ad \
+    --date=format-local:%Y%m%d%H%M.%S)
 cd "$tbname"
 find "$tzname" -print0 | TZ=UTC xargs -0r touch -h -t "$ts" --
+print -ru8 -- "archiving source"
 find "$tzname" \( -type f -o -type l \) -print0 | sort -z | \
     paxcpio -oC512 -0 -Hustar -Mdist | gzip -n9 >"../$tzname.tgz"
+print -ru8 -- "moving source tarballs into place"
 rm -rf "$tzname"  # to save space
+rm -f src.tgz
+ln "../$tzname.tgz" src.tgz
+
+# shove dependencies’ sources into place
+rm -f deps-src.zip
+cd ..
+found=0
+for x in *-sources-of-dependencies.zip; do
+	[[ -e $x && -f $x && ! -h $x && -s $x ]] || continue
+	if (( found++ )); then
+		errmsg 'multiple depsrcs archives found:' \
+		    "$(ls -l *-sources-of-dependencies.zip)"
+		break
+	fi
+	fn=$x
+done
+(( found == 1 )) || die 'could not link dependency sources'
+ln "$fn" mksrc/deps-src.zip
+print -ru8 -- "mksrc.sh finished successfully"
